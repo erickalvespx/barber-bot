d35638a455e8a68aaec221038ce616ac279d760f
Erick <erickalvesxp478@gmail.com>
2026-07-05 16:49:43 -0300

    adicao de modificaçoes para linux

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const client = new Client({
     webVersionCache: {
         type: 'remote',
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+    },
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
